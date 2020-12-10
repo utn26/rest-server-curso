e13fb5b45a5c17cf78c1fa29d12b2d6f409cbf7b
Ramiro <ramiromartinez.utn@gmail.com>
2020-12-10 12:02:26 -0300

    variable de entorno mogo

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cafe-user:UTN123456@cluster0.wznzb.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
